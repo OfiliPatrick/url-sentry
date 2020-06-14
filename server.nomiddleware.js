0748527cd60d9e2965d1963b49f27b1253f148ba
Patrick Ofili <patrickofili16@gmail.com>
2020-06-15 00:25:36 +0100

    initial commit

---
+++ server.nomiddleware.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
+const sendgridTransport = require("nodemailer-sendgrid-transport");
+const app = express();
+require("dotenv").config();
+//middle ware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+let today = new Date();
+let date =
+  today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+let time =
+  today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+let dateTime = date + " " + time;
+
+// configure transporter
+const transporter = nodemailer.createTransport(
+  sendgridTransport({
+    auth: {
+      api_key: process.env.SENDGRID_API_KEY,
+    },
+  })
+);
+
+let emailBody = `
+  <p> URL Sentry has detected access!</p>
+  <h3 > Access Details</h3>
+  <ul>
+  <li> URL: azure.com</li>
+  <li> Access Type: Unsecure</li>
+  <li> Date Accessed: ${dateTime}</li>
+  </ul>
+  <p> &copy; Patrick Ofili 2020</p>
+    `;
+
+let mailOptions = {
+  to: process.env.TO_EMAIL,
+  from: process.env.FROM_EMAIL,
+  subject: "SITE ACCESS DETECTED",
+  html: emailBody,
+};
+
+app.get("/", (req, res) => {
+  transporter.sendMail(mailOptions, (err, data) => {
+    if (err) {
+      return console.log(err);
+    }
+    res.send("<h3>SORRY RESTRICTED URL</h3>");
+  });
+});
+
+const PORT = 80;
+
+app.listen(PORT, () => {
+  console.log(`server is running at ${PORT}`);
+});
